c0980a84e2a66d4b8b08a9dc4c8e94fcbb4cecf3
William Costa <dev.williamcosta@gmail.com>
2017-06-05 19:08:51 -0300

    Transform ProductList in Class Component.

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ProductItem from './ProductListItem.jsx'
 
-export default props => (
-    <section className="product-grid">
-        <h1 className="product-grid__title">{props.listTitle}</h1>
-        <ul className="product-list">
-            <ProductItem />
-        </ul>
-    </section>
-)
+export default class ProductList extends Component {
+    render() {
+        return (
+            <section className="product-grid">
+                <h1 className="product-grid__title">{this.props.listTitle}</h1>
+                <ul className="product-list">
+                    <ProductItem />
+                </ul>
+            </section>
+        )
+    }
+}
