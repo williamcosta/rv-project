5626dec4065bfedaecf1dd76c57fadb0a98d168c
William Costa <dev.williamcosta@gmail.com>
2017-06-06 22:52:12 -0300

    Fix import react lowercase.

--- src/components/buttonFilter.jsx
+++ src/components/buttonFilter.jsx
@@ -1,4 +1,4 @@
-import React from 'React'
+import React from 'react'
 
 const ButtonFilter = props => (
     <button type="button" role="button" className="button [ button--dark ] filter__button">Todos os Produtos</button>
